abdcdafec01bc81466879aa195350d589c91c1e2
Mason1708 <824624@mystma.org>
2026-04-07 15:05:38 -0500

    spaceship

---
+++ practice/3-1/spaceship.py
@@ -0,0 +1,36 @@
+countdown = (input("number of seconds until launch: "))
+
+print("Countdown set to:", countdown, "seconds")
+
+countdown = (input("how mnay seconds until launch: "))
+
+oxygen_tanks = (input("how many oxygen tanks are on board: "))
+
+food_packs = (input("how many food packs are on board: "))
+
+water_packs = (input("how many water packs are on board: "))
+
+print("\nMission supplies recorded!")
+
+countdown = 5
+
+oxygen_tanks = 2
+
+food_packs = 30
+
+water_packs= 100
+
+
+food_packs = 50
+
+water_packs = 100
+
+oxygen_tanks = 12
+
+print("Updated Supplies:")
+
+print("Food packs:", food_packs)
+
+print("Water packs:",  water_packs)
+
+print("Oxygen Tanks:", oxygen_tanks)
